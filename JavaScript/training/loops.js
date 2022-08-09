084140dbdf8d9d3656c883eb9097f5fd8907fc08
Mogade <Atharva.Mogade@wkfs-frc.local>
2022-08-09 17:02:25 +0530

    loops.js added

---
+++ JavaScript/training/loops.js
@@ -0,0 +1,15 @@
+// Uncomment the loops.js script from index.html 
+
+/* -------------------------------- FOR LOOP -------------------------------- */
+
+for (let i = 0; i < 10; i++) {
+    console.log(i + 1);
+}
+
+/* ------------------------------- WHILE LOOP ------------------------------- */
+
+let i = 10;
+while (i < 20) {
+    console.log(i + 1);
+    i++;
+}
